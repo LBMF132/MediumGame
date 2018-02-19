6cfadddfa05f235561d97e61e8a313b6ee07462d
LBMF132 <35092274+LBMF132@users.noreply.github.com>
2018-02-19 09:13:15 +0300

    Initial add

---
+++ WizardGame.java
@@ -0,0 +1,45 @@
+/*  
+* To change this template, choose Tools | Templates  
+* and open the template in the editor.  
+*/
+
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+
+
+public class WizardGame extends BasicGame {
+	public WizardGame() {
+		super("Wizard game");
+	}
+	private TiledMap grassMap;
+	private String dataPath = "C:\\users\\19lfreeman\\desktop\\map\\";
+	public static void main(String[] arguments) {
+		try {
+			AppGameContainer app = new AppGameContainer(new WizardGame());
+			app.setDisplayMode(500, 400, false);
+			app.start();
+		} catch (SlickException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override	
+    public void init(GameContainer container) throws SlickException
+    {
+    	
+    	
+    	grassMap = new TiledMap(dataPath+"test2.tmx");
+    }
+
+	@Override
+	public void update(GameContainer container, int delta) throws SlickException {
+	}
+
+	public void render(GameContainer container, Graphics g) throws SlickException {
+		grassMap.render(0, 0);
+
+	}
+}
